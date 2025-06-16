a4093ed134a86e6d9d09f749a59fe51ef5e92572
Nadia Lutz <na.lutz@outlook.com>
2025-06-16 13:18:01 +0200

    about us done

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,9 @@
+import About from "../components/AboutUs/About";
+
 export default function AboutUs() {
     return (
         <>
-        
-        <h1>hej</h1>
+      <About/>
         </>
     )
 }
