e2f136ab1a201f591f3a13c3da6a753aa81bd106
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-06-16 11:28:46 +0200

    HTML for contactform

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,23 +6,32 @@ export default function Contact() {
     return (
         <>
         
-        <h1>Contact</h1>
+        <h1>GET IN CONTACT WITH US</h1>
 
         <Form method="post" >
             <div className="form-group">
-                <label htmlFor="name">Name:</label>
+                <label htmlFor="name">Full name:</label>
                 <p>{errors && errors?.name?.errors[0]}</p>
 
                 <input type="text" name="name" placeholder="Name" />
             </div>
+
             <div className="form-group">
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="email">Email</label>
                 <p>{errors && errors?.email?.errors[0]}</p>
 
                 <input type="email" name="email" placeholder="Email" />
             </div>
+
             <div className="form-group">
-                <label htmlFor="message">Message:</label>
+                <label htmlFor="subject">Subject</label>
+                <p>{errors && errors?.subject?.errors[0]}</p>
+
+                <input type="text" name="subject" placeholder="Subject" />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="message">Message</label>
                 <p>{errors && errors?.message?.errors[0]}</p>
                 <textarea type="message" name="message" placeholder="Email" />
             </div>
