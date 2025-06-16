03870e654e596ba396e4091398831e2694efb8bb
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-06-16 14:04:12 +0200

    Form style

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,41 +1,39 @@
 
-
 import { Form, useActionData } from "react-router";
 
-export default function Contact() {
+export default function ContactForm() {
     const errors = useActionData();
 
     return (
         <>
         
-        <h1>GET IN CONTACT WITH US</h1>
 
-        <Form method="post" >
+        <Form method="post"  className="contactform">
             <div className="form-group">
-                <label htmlFor="name">Full name:</label>
+                <label htmlFor="name" className="contactform__label">Full name:</label>
                 <p>{errors && errors?.name?.errors[0]}</p>
 
-                <input type="text" name="name" placeholder="Name" />
+                <input type="text" name="name" placeholder="Name" className="contactform__input"/>
             </div>
 
             <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <p>{errors && errors?.email?.errors[0]}</p>
 
-                <input type="email" name="email" placeholder="Email" />
+                <input type="email" name="email" placeholder="Email" className="contactform__input" />
             </div>
 
             <div className="form-group">
                 <label htmlFor="subject">Subject</label>
                 <p>{errors && errors?.subject?.errors[0]}</p>
 
-                <input type="text" name="subject" placeholder="Subject" />
+                <input type="text" name="subject" placeholder="Subject" className="contactform__input"/>
             </div>
 
             <div className="form-group">
                 <label htmlFor="message">Message</label>
                 <p>{errors && errors?.message?.errors[0]}</p>
-                <textarea type="message" name="message" placeholder="Email" />
+                <textarea type="message" name="message" placeholder="Email" className="contactform__message"/>
             </div>
             <button type="submit">Submit</button>
         </Form>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,15 @@
-import Contact from "../components/Contact/ContactForm";
+
+import ContactForm from "../components/Contact/ContactForm";
 
 export default function Contact() {
    
     return (
         <>
-         <Contact />
+        <div className="contact">
+        <h1>GET IN CONTACT WITH US</h1>
+
+         <ContactForm />
+         </div>
         </>
     )
 }
