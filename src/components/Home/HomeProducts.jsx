46008b1e2195e2cb400db3e25e31c03ecddbf150
iamayac <ayachristia@gmail.com>
2025-06-16 14:03:16 +0200

    productcard dynamic for main and list page

--- src/components/Home/HomeProducts.jsx
+++ src/components/Home/HomeProducts.jsx
@@ -1,14 +1,36 @@
+// import { useLoaderData } from "react-router"
+import Button from "../UniversalComponents/Button";
+import ProductCard from "../UniversalComponents/ProductCard";
+
 export default function HomeProducts() {
+    // const products = useLoaderData()
+
     return (
         <>
         
         <section className="homeproducts">
             <header className="homeproducts__header">
             <h1 className="homeproducts__headline">popular products</h1>
+            <Button
+            text="See more products"
+            path="list"
+            classname="homeproducts"
+            />
             </header>
 
             <section className="homeproducts__products">
-                
+                <ProductCard
+                pagefooter="home"
+                />
+                <ProductCard
+                pagefooter="home"
+                />
+                <ProductCard
+                pagefooter="home"
+                />
+                <ProductCard
+                pagefooter="home"
+                />
             </section>
         </section>
         </>
--- src/components/UniversalComponents/Button.jsx
+++ src/components/UniversalComponents/Button.jsx
@@ -1,7 +1,11 @@
-export default function Button ({text, path, classname}) {
-return (
- 
-<Link to={`/{path}`} classname={`button button__${classname}`}> {text} </Link>
-)
+import { Link } from "react-router";
+
+
+export default function Button({text, path, productId, classname}) {
+
+    return (
+    
+    <Link to={`/{path}/{productId}`} className={`button button__${classname}`}> {text} </Link>
+    )
  
 }
