d45996ad4685fde5eb70c2f24481bb763a125721
iamayac <ayachristia@gmail.com>
2025-06-16 12:47:47 +0200

    start af homeproducts section

---
+++ src/components/Home/HomeProducts.jsx
@@ -0,0 +1,16 @@
+export default function HomeProducts() {
+    return (
+        <>
+        
+        <section className="homeproducts">
+            <header className="homeproducts__header">
+            <h1 className="homeproducts__headline">popular products</h1>
+            </header>
+
+            <section className="homeproducts__products">
+                
+            </section>
+        </section>
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Header from "../components/UniversalComponents/Header";
 import Intro from "../components/Home/Intro";
 import Newsletter from "../components/Home/Newsletter";
+import HomeProducts from "../components/Home/HomeProducts";
+
 
 export default function Home(){
 
@@ -9,10 +11,11 @@ export default function Home(){
     return(
         <>
         <section className="home">
-        <h1 className="home__headline">Home</h1>
-        </section>
+        
+        <HomeProducts/>
         <Intro/>
         <Newsletter/>
+        </section>
         </>
     )
 }
