b909a7bd42cb298ab46f69953af1c916660f45ec
Sidsel Thomsen <U2469819@rts-365.dk>
2025-06-17 13:02:59 +0200

    headline everywhere

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -3,6 +3,7 @@ import About from "../components/AboutUs/About";
 export default function AboutUs() {
     return (
         <>
+        <h1 className="headline">OUR HISTORY</h1>
       <About/>
         </>
     )
