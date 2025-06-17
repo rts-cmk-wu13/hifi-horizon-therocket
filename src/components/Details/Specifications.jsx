e7ff6dfcf8c0ff10cce9fa2c15dfe1a4af45c837
Sidsel Thomsen <U2469819@rts-365.dk>
2025-06-17 11:23:19 +0200

    aaaaaalmost

---
+++ src/components/Details/Specifications.jsx
@@ -0,0 +1,14 @@
+export default function Specifications() {
+  return (
+    <>
+        <h2>Product Specifications</h2>
+        
+        <ul>
+            <li>yo</li>
+            <li>yo</li>
+            <li>yo</li> 
+            
+        </ul>
+    </>
+  );
+}
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,14 +1,15 @@
 import { useParams } from "react-router"
+import Specifications from "../components/Details/specifications";
 
 export default function Detail(){
 
     const {id}= useParams();
 
- 
-
     return  (
         <>
         <h2>DETAILS</h2>
+
+        <Specifications/>
         </>
     )
 }
