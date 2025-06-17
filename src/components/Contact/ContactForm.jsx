0f85054ed4564e80cd1f024d756df4005e51caf3
Jan-C-Nielsen <U2488247@rts-365.dk>
2025-06-17 11:08:44 +0200

    post works

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,16 +1,45 @@
 
 import { Form, useActionData } from "react-router";
+import { useState } from 'react';
 
 export default function ContactForm() {
     const errors = useActionData();
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+console.log('Form data:', data);
+
+        const response = await fetch('http://localhost:4000/contacts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const result = await response.json();
+        console.log(result);
+        if (!response.ok) {
+            // Handle error, e.g., show a message to the user
+            //throw new Error('Login failed');
+            setError(result.message || 'Submit failed');
+            console.error('Submit failed:', result);
+            return;
+        }
+     
+
+    }
+
 
     return (
         <>
         
-
-        <Form method="post"  className="contactform">
+        <Form onSubmit={handleSubmit} className="contactform">
             <div className="form-group">
-                <label htmlFor="name" className="contactform__label">Full name:</label>
+                <label htmlFor="name" className="contactform__label">Full name</label>
                 <p>{errors && errors?.name?.errors[0]}</p>
 
                 <input type="text" name="name" placeholder="Name" className="contactform__input"/>
@@ -33,7 +62,7 @@ export default function ContactForm() {
             <div className="form-group">
                 <label htmlFor="message">Message</label>
                 <p>{errors && errors?.message?.errors[0]}</p>
-                <textarea type="message" name="message" placeholder="Email" className="contactform__message"/>
+                <textarea type="message" name="message" placeholder="Message" className="contactform__message"/>
             </div>
             <button type="submit">Submit</button>
         </Form>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,10 +5,9 @@ export default function Contact() {
    
     return (
         <>
-        <div className="contact">
         <h1>GET IN CONTACT WITH US</h1>
-
-         <ContactForm />
+        <div className="contact">
+               <ContactForm />
          </div>
         </>
     )
