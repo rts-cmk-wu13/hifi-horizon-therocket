9034ee448d964b469b6498e66871064ecd00838b
iamayac <ayachristia@gmail.com>
2025-06-23 08:49:46 +0200

    syntax error list

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,12 +1,8 @@
-
-
-
-export default function List() {
-
 import ListNavigation from "../components/List/ListNavigation";
 import ProductRendering from "../components/List/ProductRendering";
 import { useLoaderData } from "react-router";
 import Chat from "../components/List/Chat"; 
+
 
 export default function List() {
   const products = useLoaderData();
