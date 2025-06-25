2e1250ca9eba3076afaed9e7dd45081472232fe2
Sidsel Thomsen <U2469819@rts-365.dk>
2025-06-25 13:57:00 +0200

    pic

--- src/components/Details/ProductCompare.jsx
+++ src/components/Details/ProductCompare.jsx
@@ -1,4 +1,6 @@
 import CreekEvo from '../../../public/cd_afspillere/creek_evo_cd.jpg';
+import Exp_2010S_CD from '../../../public/cd_afspillere/Exp_2010S_CD.gif';
+import creek_classic_cd from '../../../public/cd_afspillere/creek_classic_cd.jpg';
 
 export default function ProductCompare() {
   return (
@@ -11,8 +13,8 @@ export default function ProductCompare() {
             <tr className="spec-table__first-row">
                 <th className="spec-table__first-row"></th>
                 <td className="spec-table__first-row"><img className="comparison_pics" src={CreekEvo} alt="" /></td>
-                <td className="spec-table__first-row"><img className="comparison_pics" src={CreekEvo} alt="" /></td>
-                <td className="spec-table__first-row"><img className="comparison_pics" src={CreekEvo} alt="" /></td>
+                <td className="spec-table__first-row"><img className="comparison_pics" src={Exp_2010S_CD} alt="" /></td>
+                <td className="spec-table__first-row"><img className="comparison_pics" src={creek_classic_cd} alt="" /></td>
             </tr>
             <tr>
                 <th>xxxxxxxx</th>
